11a11c569b0bf082b38a0067abc9b8ca7d5f6e95
kunhi <kunhimohamed6@gmail.com>
2022-12-28 14:27:53 +0400

    feat: two excercise

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height_of_ball_drop = int(input())
+
+for index, value in enumerate(range(10)):
+    the_required_result = height_of_ball_drop*(3/5)
+    print(index+1, round(the_required_result, 4))
+    height_of_ball_drop = the_required_result
