1bc2ff0d4ab321f750db423be115edd89f09d064
kunhi <kunhimohamed6@gmail.com>
2022-12-29 12:35:58 +0400

    feat: one program with wrong one

--- Work/extra_payments.py
+++ Work/extra_payments.py
@@ -1,13 +1,19 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.8
+
+# this is wrong program
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-
+iteration_count = 0
 while principal > 0:
+    iteration_count += 1
+    print(iteration_count)
+    if iteration_count<=12:
+        payment = payment + 1000
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
